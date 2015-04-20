5dada1d8f7f28a5f24ab570044fd197bf7674d74
Neil <neil-lu@graduate.uwa.edu.au>
2015-04-20 17:38:17 +0800

    style input area

--- client/javascripts/index.js
+++ client/javascripts/index.js
@@ -1,55 +1,62 @@
 // CommentCoreLibrary (//github.com/jabbany/CommentCoreLibrary) - Licensed under the MIT license
 
-$('#msg').width($('#background').width());
-$(window).on('resize', function(e){
-  $('#msg').width($('#background').width());
-});
+$(document).on('ready', function(){
+  $('#msg').width($('#background').width()*0.30);
+  $('#msg').height($('#send').height()*0.8);
 
-var socket = io();
-var cm = new CommentManager(document.getElementById('danmu'));
+  $(window).on('resize', function(e){
+    $('#msg').width($('#background').width()*0.30);
+    $('#msg').height($('#send').height()*0.8);
+  });
 
-cm.options.global.scale = 5;
-cm.options.scroll.scale = 2;
+  var socket = io();
+  var cm = new CommentManager(document.getElementById('danmu'));
 
-cm.init();
-cm.start();
+  cm.options.global.scale = 5;
+  cm.options.scroll.scale = 2;
 
-socket.on('danmu', function(danmu){
-  cm.send(danmu);
-});
+  cm.init();
+  cm.start();
 
-$('form').on('submit', function(e){
-  e.preventDefault();
-  var danmu = {
-    "mode": 1,
-    "stime": + 10,
-    "size": 25,
-    "color": "0xffffff",
-    "text": $('#msg').val(),
+  socket.on('danmu', function(danmu){
+    cm.send(danmu);
+  });
+
+  function sendDanmu(e){
+    e.preventDefault();
+    var danmu = {
+      "mode": 1,
+      "stime": + 10,
+      "size": 25,
+      "color": "0xffffff",
+      "text": $('#msg').val(),
+    }
+    $('#msg').val('');
+    socket.emit('danmu', danmu);
+    $.post(window.location.href + 'api/danmus', danmu)
   }
-  $('#msg').val('');
-  socket.emit('danmu', danmu);
-  $.post(window.location.href + 'api/danmus', danmu)
-});
+  $('#send').on('click', sendDanmu);
+  $('form').on('submit', sendDanmu);
 
-function fetchDanmus(){
-  if(!cm.runline.length){
-    $.ajax({
-      url: window.location.href + 'api/danmus?filter=%7B%22order%22%3A%20%22stime%20DESC%22%7D',
-      success: function(data){
-        data.forEach(function(danmu){
-          (function(danmu){
-            setTimeout(function(){
-              danmu.color = '0x' + parseInt(danmu.color).toString(16);
-              cm.send(danmu);
-            }, Math.random() * 100000 + 3000);
-          })(danmu);
-        });
-      }
-    });
-  }else {
-    return;
+  function fetchDanmus(){
+    if(!cm.runline.length){
+      $.ajax({
+        url: window.location.href + 'api/danmus?filter=%7B%22order%22%3A%20%22stime%20DESC%22%7D',
+        success: function(data){
+          data.forEach(function(danmu){
+            (function(danmu){
+              setTimeout(function(){
+                danmu.color = '0x' + parseInt(danmu.color).toString(16);
+                cm.send(danmu);
+              }, Math.random() * 100000 + 3000);
+            })(danmu);
+          });
+        }
+      });
+    }else {
+      return;
+    }
   }
-}
 
-setInterval(fetchDanmus, 10000);
+  setInterval(fetchDanmus, 10000);
+});
